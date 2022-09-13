a4be53be6474e6ea6af87f933e85455e3640fd7a
oleg ostrovski <oleg.ostrovski@gmail.com>
2022-09-13 20:41:47 +0300

    add header + content

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,93 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <div className="wrapper clear">
+            <header>
+                <div className={"headerLeft"}>
+                    <img width={40} height={40} src="./img/logo.png" alt="logo"/>
+                    <div>
+                        <h3>React Sneakers</h3>
+                        <p>Магазин лучших кроссовок</p>
+                    </div>
+                </div>
+
+                <ul className={"headerRight"}>
+                    <li>
+                        <img width={18} height={18} src="./img/cart.svg" alt="cart"/>
+                        <span>1205 руб</span>
+                    </li>
+                    <li>
+                        <img width={18} height={18} src="./img/user.svg" alt="user"/>
+                    </li>
+                </ul>
+            </header>
+            <div className="content">
+                <h1>Все кроссовки</h1>
+
+
+                <div className="sneakers">
+                    <div className="card">
+                        <img width={133} height={112} src={'/img/sneakers/1.jpg'} alt={'sneakers'}/>
+                        <h5>Мужские Кроссовки Nike Blazer Mid Suede</h5>
+                        <div className={"cardButton"}>
+                            <div className={"cardPriсe"}>
+                                <span>Цена:</span>
+                                <b>12 999 руб.</b>
+                            </div>
+                            <button className={"button"}>
+                                <img width={11} height={11} src={'/img/plus.svg'} alt={'plus'}/>
+                            </button>
+                        </div>
+                    </div>
+
+                    <div className="card">
+                        <img width={133} height={112} src={'/img/sneakers/2.jpg'} alt={'sneakers'}/>
+                        <h5>Мужские Кроссовки Nike Blazer Mid Suede</h5>
+                        <div className={"cardButton"}>
+                            <div className={"cardPriсe"}>
+                                <span>Цена:</span>
+                                <b>12 999 руб.</b>
+                            </div>
+                            <button className={"button"}>
+                                <img width={11} height={11} src={'/img/plus.svg'} alt={'plus'}/>
+                            </button>
+                        </div>
+                    </div>
+
+                    <div className="card">
+                        <img width={133} height={112} src={'/img/sneakers/3.jpg'} alt={'sneakers'}/>
+                        <h5>Мужские Кроссовки Nike Blazer Mid Suede</h5>
+                        <div className={"cardButton"}>
+                            <div className={"cardPriсe"}>
+                                <span>Цена:</span>
+                                <b>12 999 руб.</b>
+                            </div>
+                            <button className={"button"}>
+                                <img width={11} height={11} src={'/img/plus.svg'} alt={'plus'}/>
+                            </button>
+                        </div>
+                    </div>
+
+                    <div className="card">
+                        <img width={133} height={112} src={'/img/sneakers/4.jpg'} alt={'sneakers'}/>
+                        <h5>Мужские Кроссовки Nike Blazer Mid Suede</h5>
+                        <div className={"cardButton"}>
+                            <div className={"cardPriсe"}>
+                                <span>Цена:</span>
+                                <b>12 999 руб.</b>
+                            </div>
+                            <button className={"button"}>
+                                <img width={11} height={11} src={'/img/plus.svg'} alt={'plus'}/>
+                            </button>
+                        </div>
+                    </div>
+                </div>
+
+
+            </div>
+        </div>
+    );
 }
 
 export default App;
