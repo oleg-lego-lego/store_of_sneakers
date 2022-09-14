4b2483cad529854c95d5820c2ee91276d741b9e7
oleg ostrovski <oleg.ostrovski@gmail.com>
2022-09-14 11:15:15 +0300

    add search + input

--- src/App.tsx
+++ src/App.tsx
@@ -5,7 +5,7 @@ function App() {
         <div className="wrapper clear">
             <header>
                 <div className={"headerLeft"}>
-                    <img width={40} height={40} src="./img/logo.png" alt="logo"/>
+                    <img width={40} height={40} src="/img/logo.png" alt="logo"/>
                     <div>
                         <h3>React Sneakers</h3>
                         <p>Магазин лучших кроссовок</p>
@@ -14,16 +14,22 @@ function App() {
 
                 <ul className={"headerRight"}>
                     <li>
-                        <img width={18} height={18} src="./img/cart.svg" alt="cart"/>
+                        <img width={18} height={18} src="/img/cart.svg" alt="cart"/>
                         <span>1205 руб</span>
                     </li>
                     <li>
-                        <img width={18} height={18} src="./img/user.svg" alt="user"/>
+                        <img width={18} height={18} src="/img/user.svg" alt="user"/>
                     </li>
                 </ul>
             </header>
             <div className="content">
-                <h1>Все кроссовки</h1>
+                <div>
+                    <h1>Все кроссовки</h1>
+                    <div className="searchBlock">
+                        <img src="/img/search.svg" alt="search"/>
+                        <input placeholder={'Поиск...'}/>
+                    </div>
+                </div>
 
 
                 <div className="sneakers">
