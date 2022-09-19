7f8ce4bcc6846d5f3ea1e986bd927284c3116c8d
oleg ostrovski <oleg.ostrovski@gmail.com>
2022-09-19 21:42:05 +0300

    add fetch in API serves

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,33 @@
-import React, {useState} from 'react';
+import React, {useEffect, useState} from 'react';
 import {Card} from "./components/Card/Card";
 import {Header} from "./components/Header";
 import {Drawer} from "./components/Drawer";
 
+export type ItemsType = {
+    imageURL: string
+    title: string
+    price: number
+}
+
 const arr = [
-    {title: 'Мужские Кроссовки Nike Blazer Mid Suede', price: 12999, imageURL: 'img/sneakers/1.jpg'},
-    {title: 'Мужские Кроссовки Nike Air Max 270', price: 15600, imageURL: 'img/sneakers/2.jpg'}
+    {"title": "Мужские Кроссовки Nike Blazer Mid Suede", "price": 12999, "imageURL": "img/sneakers/1.jpg"},
+    {"title": "Мужские Кроссовки Nike Air Max 270", "price": 15600, "imageURL": "img/sneakers/2.jpg"},
 ]
 
 function App() {
+    const [items, setItems] = useState<ItemsType[]>([])
+
     const [cartOpened, setCartOpened] = useState(false)
+
+    useEffect(() => {
+        fetch('https://6328ab4ecc4c264fdedfb384.mockapi.io/items')
+            .then(res => {
+                return res.json()
+            })
+            .then(json => {
+                setItems(json)
+            })
+    }, [])
 
     return (
         <div className="wrapper clear">
@@ -25,7 +43,7 @@ function App() {
                 </div>
 
                 <div className="sneakers">
-                    {arr.map(el => <Card title={el.title} price={el.price} imageURL={el.imageURL}/>)}
+                    {items.map(el => <Card title={el.title} price={el.price} imageURL={el.imageURL}/>)}
 
                 </div>
             </div>
--- src/components/Card/Card.tsx
+++ src/components/Card/Card.tsx
@@ -37,3 +37,6 @@ export const Card = (props: CardPropsType) => {
         </div>
     );
 };
+
+
+
