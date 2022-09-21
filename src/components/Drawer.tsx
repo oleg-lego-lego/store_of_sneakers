5da9ce712c3c65b26698268f17d64bd4019ee9b0
oleg ostrovski <oleg.ostrovski@gmail.com>
2022-09-21 19:16:54 +0300

    add logic in basket

--- src/components/Drawer.tsx
+++ src/components/Drawer.tsx
@@ -7,7 +7,7 @@ type DrawerPropsType = {
     items: any
 }
 
-export const Drawer = ({ onClose, items, onRemove }: { onClose: any; items: ItemsType[], onRemove: (id: string) => void }) => {
+export const Drawer = ({onClose, items, onRemove}: { onClose: any; items: ItemsType[], onRemove: (id: string) => void }) => {
     return (
         <div className="overlay">
             <div className="drawer">
@@ -17,38 +17,58 @@ export const Drawer = ({ onClose, items, onRemove }: { onClose: any; items: Item
                     <img onClick={onClose} className={"removeBtn"} src="/img/btn_remove.svg" alt="btn_remove"/>
                 </h2>
 
-                <div className="items">
-                    {items.map(el => {
-                        return (
-                            <div className="cartItems">
-                                <div style={{backgroundImage: `url(${el.imageURL})`}} className="cartItemImg"></div>
+                {
+                    items.length > 0 ?
+                        <>
+                            <div className="items">
+                                {items.map(el => {
+                                    return (
+                                        <div className="cartItems">
+                                            <div style={{backgroundImage: `url(${el.imageURL})`}}
+                                                 className="cartItemImg"></div>
 
-                                {/*<img className={"cartItem"} src="/img/sneakers/1.jpg" alt="sneakers"/>*/}
-                                <div className={"cartText"}>
-                                    <p>{el.title}</p>
-                                    <b>{(el.price)}</b>
-                                </div>
-                                <img onClick={() => onRemove(el.id)} className={"removeBtn"} src="/src/img/img/btn_remove.svg" alt="btn_remove"/>
+                                            {/*<img className={"cartItem"} src="/img/sneakers/1.jpg" alt="sneakers"/>*/}
+                                            <div className={"cartText"}>
+                                                <p>{el.title}</p>
+                                                <b>{(el.price)}</b>
+                                            </div>
+                                            <img onClick={() => onRemove(el.id)} className={"removeBtn"}
+                                                 src="/src/img/img/btn_remove.svg" alt="btn_remove"/>
+                                        </div>
+                                    )
+                                })}
                             </div>
-                        )
-                    })}
-                </div>
 
-                <div className={"cartTotalBlock"}>
-                    <ul>
-                        <li>
-                            <span>Итого:</span>
-                            <div></div>
-                            <b>21 498 руб</b>
-                        </li>
-                        <li>
-                            <span>Налог 5%:</span>
-                            <div></div>
-                            <b>1074 руб.</b>
-                        </li>
-                    </ul>
-                    <button className={"greenButton"}>Оформить заказ <img src="/src/img/img/arrow.svg" alt="arrow"/></button>
-                </div>
+                            <div className={"cartTotalBlock"}>
+                                <ul>
+                                    <li>
+                                        <span>Итого:</span>
+                                        <div></div>
+                                        <b>21 498 руб</b>
+                                    </li>
+                                    <li>
+                                        <span>Налог 5%:</span>
+                                        <div></div>
+                                        <b>1074 руб.</b>
+                                    </li>
+                                </ul>
+                                <button className={"greenButton"}>Оформить заказ <img src="/src/img/img/arrow.svg"
+                                                                                      alt="arrow"/></button>
+                            </div>
+                        </>
+                        :
+                        <div className="cartEmpty">
+                            <img className="emptyImage" src={'/img/empty-cart.jpg'} alt="EmptyCart"/>
+                            <h2>{'title'}</h2>
+                            <p>{'description'}</p>
+                            <button onClick={onClose} className="greenButton">
+                                <img className="imgArrow" src="img/arrow.svg" alt="Arrow"/>Вернуться назад
+                            </button>
+                        </div>
+
+                }
+
+
             </div>
         </div>
     );
