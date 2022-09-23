270461937e4e5e8421c7a0ed5b2283819bcfa954
oleg ostrovski <oleg.ostrovski@gmail.com>
2022-09-23 16:49:22 +0300

    add ContentLoader Card

--- src/components/Card/Card.tsx
+++ src/components/Card/Card.tsx
@@ -1,6 +1,7 @@
 import React, {FC, useEffect, useState} from 'react';
 import s from './Card.module.scss'
 import {ItemsType} from "../../App";
+import ContentLoader from "react-content-loader";
 
 type CardPropsType = {
     id: string
@@ -11,11 +12,13 @@ type CardPropsType = {
     onPlus?: (obj: ItemsType) => void
     favorited?: boolean
     added?: boolean
+    loading?: boolean
 }
 
 export const Card: FC<CardPropsType> = (
     {title, price, imageURL, id, onFavorite,
-        onPlus, favorited = false, added = false}) => {
+        onPlus, favorited = false, added = false,
+    loading = false }) => {
     const [isAdded, setIsAdded] = useState(added)
     const [isFavorite, setIsFavorite] = useState(favorited)
 
@@ -43,20 +46,40 @@ export const Card: FC<CardPropsType> = (
 
     return (
         <div className={s.card}>
-            <div className={s.favorite}>
-                <img onClick={onClickFavorite} src={isFavorite ? '/img/liked.svg' : '/img/unliked.svg'} alt="unliked"/>
-            </div>
 
-            <img width={133} height={112} src={imageURL} alt={'sneakers'}/>
-            <h5>{title}</h5>
-            <div className={s.cardButton}>
-                <div className={s.cardPrise}>
-                    <span>Цена:</span>
-                    <b>{price}</b>
-                </div>
-                <img className={s.plus} onClick={onClickPlus}
-                     src={isAdded ? '/img/btn_checked.svg' : '/img/btn_plus.svg'} alt={'plus'}/>
-            </div>
+            {
+                loading ?
+                    <ContentLoader
+                        speed={2}
+                        width={165}
+                        height={250}
+                        viewBox="0 0 155 265"
+                        backgroundColor="#f3f3f3"
+                        foregroundColor="#ecebeb">
+                        <rect x="0" y="0" rx="10" ry="10" width="155" height="155"/>
+                        <rect x="0" y="167" rx="5" ry="5" width="155" height="15"/>
+                        <rect x="0" y="187" rx="5" ry="5" width="100" height="15"/>
+                        <rect x="0" y="234" rx="5" ry="5" width="80" height="25"/>
+                        <rect x="124" y="230" rx="10" ry="10" width="32" height="32"/>
+                    </ContentLoader>
+                    :
+                    <>
+                        <div className={s.favorite}>
+                            <img onClick={onClickFavorite} src={isFavorite ? '/img/liked.svg' : '/img/unliked.svg'} alt="unliked"/>
+                        </div>
+
+                        <img width={'100%'} height={135} src={imageURL} alt={'sneakers'}/>
+                        <h5>{title}</h5>
+                        <div className={s.cardButton}>
+                            <div className={s.cardPrise}>
+                                <span>Цена:</span>
+                                <b>{price}</b>
+                            </div>
+                            <img className={s.plus} onClick={onClickPlus}
+                                 src={isAdded ? '/img/btn_checked.svg' : '/img/btn_plus.svg'} alt={'plus'}/>
+                        </div>
+                    </>
+            }
         </div>
     );
 };
